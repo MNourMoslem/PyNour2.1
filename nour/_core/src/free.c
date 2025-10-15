021527f2f89cd00dce3f2f0803456487f22ed187
MNourMoslem <normoslem256@gmail.com>
2025-10-15 22:06:51 +0300

    Refactor Node_Free function to decrement reference count and free base_data correctly

--- nour/_core/src/free.c
+++ nour/_core/src/free.c
@@ -5,10 +5,13 @@
 NR_PUBLIC void
 Node_Free(Node* node){
     if (node){
+        node->ref_count--;
+        if (node->ref_count > 0) return; // Still referenced elsewhere
+
         if (node->shape) {free(node->shape);}
         if (node->strides) {free(node->strides);}
-        if (NODE_IS_OWNDATA(node)){
-            free(node->data);
+        if (NODE_IS_OWNDATA(node) && node->base_data){
+            free(node->base_data);
         }
         free(node);
     }
