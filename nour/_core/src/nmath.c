f5792ed0aaa4b8587772cb73c8f0a27e54ece713
MNourMoslem <normoslem256@gmail.com>
2025-10-16 23:10:49 +0300

    creating nmath files in src

---
+++ nour/_core/src/nmath.c
@@ -0,0 +1,4 @@
+#include "nmath.h"
+#include "niter.h"
+#include "node_core.h"
+#include "./build/generated/__nmath_generated.c"
---
+++ nour/_core/src/nmath.h
@@ -0,0 +1,8 @@
+#ifndef NOUR__CORE_SRC_NOUR_NMATH_H
+#define NOUR__CORE_SRC_NOUR_NMATH_H
+
+#include "nour/nour.h"
+#include "node_core.h"
+#include "build/generated/__nmath_generated.h"
+
+#endif
