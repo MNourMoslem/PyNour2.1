7b2c7aecc84074a6ea924230b56785d968f81798
MNourMoslem <normoslem256@gmail.com>
2025-10-21 21:49:10 +0300

    add: implement tests for NArray indexing and modify test structure

--- nour/_core/tests/main.c
+++ nour/_core/tests/main.c
@@ -4,7 +4,6 @@ int main(){
    test_allocation_test();
    test_shape_test();
    test_index_test();
-   test_multislice_test();
    test_refcount_test();
    test_boolean_mask_test();
    test_tc_test();
--- nour/_core/tests/main.h
+++ nour/_core/tests/main.h
@@ -41,7 +41,6 @@ static void run_test(const char* test_name, int num_of_tests, test_callback_t* t
 void test_allocation_test();
 void test_shape_test();
 void test_index_test();
-void test_multislice_test();
 void test_refcount_test();
 void test_boolean_mask_test();
 void test_tc_test();
