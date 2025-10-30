a1f1b16edc147bcbf7ac549c35385bd438eed661
MNourMoslem <normoslem256@gmail.com>
2025-10-30 21:28:50 +0300

    feat: add loop macros for improved node iteration

--- nour/_core/src/niter.h
+++ nour/_core/src/niter.h
@@ -19,4 +19,28 @@ NR_PUBLIC int
 NWindowIter_New(const Node* node, NWindowIter* wit, const nr_intp* window_dims,
                 const nr_intp* strides_factor, const nr_intp* dilation);
 
+#define Node_LOOP_MODED_NAMED(node, iter_name, iter_mode, code)         \
+do{                                                    \
+                                                       \
+    NIter iter_name;                                   \
+    NIter_FromNode(&iter_name, (node), (iter_mode));   \
+    NIter_ITER(&iter_name);                            \
+    while (NIter_NOTDONE(&iter_name))                  \
+    {                                                  \
+        code                                           \
+        NIter_NEXT(&iter_name);                        \
+    }                                                  \
+} while (0);
+
+
+#define Node_LOOP_MODED(node, iter_mode, code) \
+    Node_LOOP_MODED_NAMED(node, __iter, iter_mode, code)
+
+#define Node_LOOP_NAMED(node, iter_name, code) \
+    Node_LOOP_MODED_NAMED(node, iter_name, NITER_MODE_NONE, code)
+
+#define Node_LOOP(node, code) \
+    Node_LOOP_MODED(node, NITER_MODE_NONE, code)
+
+
 #endif
