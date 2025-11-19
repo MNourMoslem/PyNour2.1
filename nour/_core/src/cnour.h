61a7c323ad385972912d6a0bcf43e3e09abb0e8d
MNourMoslem <normoslem256@gmail.com>
2025-11-19 14:32:00 +0300

    feat: add cnour.h header file with necessary includes and guard definitions

---
+++ nour/_core/src/cnour.h
@@ -0,0 +1,17 @@
+#ifndef NOUR__CORE_SRC_CNOUR_H
+#define NOUR__CORE_SRC_CNOUR_H
+
+#include "nour/nour.h"
+
+#include "node_core.h"
+#include "niter.h"
+#include "ntools.h"
+#include "nerror.h"
+#include "free.h"
+#include "tc_methods.h"
+#include "node2str.h"
+#include "shape.h"
+#include "operations.h"
+#include "index.h"
+
+#endif // NOUR__CORE_SRC_CNOUR_H
