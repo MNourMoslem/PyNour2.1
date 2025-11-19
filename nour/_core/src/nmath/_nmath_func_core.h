3821f540414c4824b79fe197e3a4ecdc3de5a2b3
MNourMoslem <normoslem256@gmail.com>
2025-11-19 15:47:44 +0300

    feat: refactor dtype resolution and input validation in nmath functions

--- nour/_core/src/nmath/_nmath_func_core.h
+++ nour/_core/src/nmath/_nmath_func_core.h
@@ -12,269 +12,190 @@ typedef struct
 {
     NR_DTYPE intype;
     NR_DTYPE outtype;
-}__nmath_iotypes;
+} __nmath_iotypes;
 
-NR_PRIVATE void
-_CheckDtypes_3Nodes(__nmath_iotypes* iot, Node* c, Node* a, Node* b, int outtype, int intype){
-    NR_DTYPE at = a->dtype.dtype;
-    NR_DTYPE bt = b->dtype.dtype;
-    NR_DTYPE abt = at == bt ? at : NTools_BroadcastDtypes(at, bt);
-    NR_DTYPE outt = c ? c->dtype.dtype : abt;
-
-    if (intype == NDTYPE_NONE){
-        iot->intype = abt;
-    }
-    else if (intype == NDTYPE_FLOAT){
-        iot->intype = (abt > NR_UINT64) && (abt < NR_NUM_NUMIRC_DT) ? abt : NR_FLOAT64;  
-    }
-    else if (intype == NDTYPE_BOOL){
-        iot->intype = NR_BOOL;
-    }
-    else if (intype == NDTYPE_INT){
-        iot->intype = abt <= NR_UINT64 ? abt : NR_INT64;
-    }
-    else{
-        iot->intype = abt;
-    }
-
-    if (outtype == NDTYPE_NONE){
-        iot->outtype = iot->intype;
-    }
-    else if (outtype == NDTYPE_FLOAT){
-        iot->outtype = (outt > NR_UINT64) && (outt < NR_NUM_NUMIRC_DT) ? outt : NR_FLOAT64;  
-    }
-    else if (outtype == NDTYPE_BOOL){
-        iot->outtype = NR_BOOL;
-    }
-    else if (outtype == NDTYPE_INT){
-        iot->outtype = outt <= NR_UINT64 ? outt : NR_INT64;
-    }
-    else{
-        iot->outtype = iot->intype;
+/* Helper function to resolve dtype based on type constraints */
+NR_PRIVATE NR_DTYPE
+_ResolveDtype(NR_DTYPE dtype, int type_constraint) {
+    switch (type_constraint) {
+        case NDTYPE_NONE:
+            return dtype;
+        case NDTYPE_FLOAT:
+            return (dtype > NR_UINT64 && dtype < NR_NUM_NUMIRC_DT) ? dtype : NR_FLOAT64;
+        case NDTYPE_BOOL:
+            return NR_BOOL;
+        case NDTYPE_INT:
+            return (dtype <= NR_UINT64) ? dtype : NR_INT64;
+        default:
+            return dtype;
     }
 }
 
+/* Unified function to check and resolve dtypes */
 NR_PRIVATE void
-_CheckDtypes_2Nodes(__nmath_iotypes* iot, Node* c, Node* a, int outtype, int intype){
-    NR_DTYPE at = a->dtype.dtype;
-    NR_DTYPE outt = c ? c->dtype.dtype : at;
+_CheckDtypes(__nmath_iotypes* iot, Node* c, Node* a, Node* b, int outtype, int intype) {
+    NR_DTYPE input_dtype, output_dtype;
+    
+    if (b) {
+        /* Two input nodes */
+        NR_DTYPE at = a->dtype.dtype;
+        NR_DTYPE bt = b->dtype.dtype;
+        input_dtype = (at == bt) ? at : NTools_BroadcastDtypes(at, bt);
+    } else {
+        /* Single input node */
+        input_dtype = a->dtype.dtype;
+    }
+    
+    output_dtype = c ? c->dtype.dtype : input_dtype;
+    
+    iot->intype = _ResolveDtype(input_dtype, intype);
+    iot->outtype = _ResolveDtype((outtype == NDTYPE_NONE) ? iot->intype : output_dtype, outtype);
+}
 
-    // Resolve input type
-    if (intype == NDTYPE_NONE){
-        iot->intype = at;
-    }
-    else if (intype == NDTYPE_FLOAT){
-        iot->intype = (at > NR_UINT64) && (at < NR_NUM_NUMIRC_DT) ? at : NR_FLOAT64;  
-    }
-    else if (intype == NDTYPE_BOOL){
-        iot->intype = NR_BOOL;
-    }
-    else if (intype == NDTYPE_INT){
-        iot->intype = at <= NR_UINT64 ? at : NR_INT64;
-    }
-    else{
-        iot->intype = at;
-    }
-
-    // Resolve output type
-    if (outtype == NDTYPE_NONE){
-        iot->outtype = iot->intype;
-    }
-    else if (outtype == NDTYPE_FLOAT){
-        iot->outtype = (outt > NR_UINT64) && (outt < NR_NUM_NUMIRC_DT) ? outt : NR_FLOAT64;  
-    }
-    else if (outtype == NDTYPE_BOOL){
-        iot->outtype = NR_BOOL;
-    }
-    else if (outtype == NDTYPE_INT){
-        iot->outtype = outt <= NR_UINT64 ? outt : NR_INT64;
-    }
-    else{
-        iot->outtype = iot->intype;
+/* Convert type category to string */
+NR_PRIVATE void
+_IOType_AsString(int iotype, char dst[]) {
+    switch (iotype) {
+        case NDTYPE_FLOAT:
+            strcpy(dst, "float");
+            break;
+        case NDTYPE_BOOL:
+            strcpy(dst, "boolean");
+            break;
+        case NDTYPE_INT:
+            strcpy(dst, "integer");
+            break;
+        default:
+            strcpy(dst, "unknown");
+            break;
     }
 }
 
-NR_PRIVATE void
-_IOType_AsString(int iotype, char dst[]){
-    if (iotype == NDTYPE_FLOAT){
-        strcpy(dst, "float");  
+/* Validate input types if not castable */
+NR_PRIVATE int
+_ValidateInputTypes(Node* a, Node* b, int intype, int in_castable, char* func_name) {
+    if (in_castable) {
+        return 1; /* No validation needed */
     }
-    else if (iotype == NDTYPE_BOOL){
-        strcpy(dst, "boolean");  
-    }
-    else if (iotype == NDTYPE_INT){
-        strcpy(dst, "integer");  
-    }
-    else{
-        strcpy(dst, "unknown");  
-    }
-}
-
-NR_PRIVATE Node*
-_NMath_Func_2in1out(Node* c, Node* a, Node* b, NFunc func_list[], int outtype, int intype, int in_castable, char* func_name){
-    if (!in_castable){
-        int atype = NDtype_GetDtypeType(a->dtype.dtype);
-        int btype = NDtype_GetDtypeType(b->dtype.dtype);
-        if (atype != intype || btype != intype){
-            char intype_str[20];
-            char adtype_str[20];
+    
+    int atype = NDtype_GetDtypeType(a->dtype.dtype);
+    if (atype != intype) {
+        char intype_str[20], adtype_str[20];
+        _IOType_AsString(intype, intype_str);
+        NDtype_AsString(a->dtype.dtype, adtype_str);
+        
+        if (b) {
             char bdtype_str[20];
-            
-            NDtype_AsString(intype, intype_str);
-            NDtype_AsString(a->dtype.dtype, adtype_str);
             NDtype_AsString(b->dtype.dtype, bdtype_str);
-
-            NError_RaiseError(NError_TypeError, "%s operation not supported for input types %s and %s. Expected input types of type %s.", 
-                func_name,
-                adtype_str,
-                bdtype_str,
-                intype_str
-            );
-            return NULL;
+            int btype = NDtype_GetDtypeType(b->dtype.dtype);
+            if (btype != intype) {
+                NError_RaiseError(NError_TypeError, 
+                    "%s operation not supported for input types %s and %s. Expected input types of type %s.",
+                    func_name, adtype_str, bdtype_str, intype_str);
+                return 0;
+            }
+        } else {
+            NError_RaiseError(NError_TypeError,
+                "%s operation not supported for input type %s. Expected input type %s.",
+                func_name, adtype_str, intype_str);
+            return 0;
         }
     }
+    
+    if (b) {
+        int btype = NDtype_GetDtypeType(b->dtype.dtype);
+        if (btype != intype) {
+            char intype_str[20], bdtype_str[20];
+            _IOType_AsString(intype, intype_str);
+            NDtype_AsString(b->dtype.dtype, bdtype_str);
+            NError_RaiseError(NError_TypeError,
+                "%s operation not supported for input type %s. Expected input type %s.",
+                func_name, bdtype_str, intype_str);
+            return 0;
+        }
+    }
+    
+    return 1;
+}
 
+/* Cast node to target type if needed */
+NR_PRIVATE Node*
+_CastNodeIfNeeded(Node* node, NR_DTYPE target_type, int* copy_flag) {
+    if (node->dtype.dtype != target_type) {
+        Node* cast_node = Node_ToType(NULL, node, target_type);
+        if (cast_node) {
+            *copy_flag = 1;
+        }
+        return cast_node;
+    }
+    return node;
+}
 
-    int copy_a = 0, copy_b = 0;
-    int cast_out_to_c = 0;
-    int func_idx;
+/* Clean up copied nodes */
+NR_PRIVATE void
+_CleanupCopiedNodes(Node* a, Node* b, int copy_a, int copy_b) {
+    if (copy_a) Node_Free(a);
+    if (copy_b) Node_Free(b);
+}
+
+/* Unified function for both 1-input and 2-input operations */
+NR_PRIVATE Node*
+_NMath_Func_Core(Node* c, Node* a, Node* b, NFunc func_list[], int outtype, int intype, int in_castable, char* func_name) {
+    /* Validate input types */
+    if (!_ValidateInputTypes(a, b, intype, in_castable, func_name)) {
+        return NULL;
+    }
+    
+    /* Resolve data types */
     __nmath_iotypes iot;
-     _CheckDtypes_3Nodes(&iot, c, a, b, outtype, intype);
-
-    if (a->dtype.dtype != iot.intype){
-        a = Node_ToType(NULL, a, iot.intype);
-        if (!a){
+    _CheckDtypes(&iot, c, a, b, outtype, intype);
+    
+    /* Cast input nodes if needed */
+    int copy_a = 0, copy_b = 0;
+    a = _CastNodeIfNeeded(a, iot.intype, &copy_a);
+    if (!a) {
+        return NULL;
+    }
+    
+    if (b) {
+        b = _CastNodeIfNeeded(b, iot.intype, &copy_b);
+        if (!b) {
+            if (copy_a) Node_Free(a);
             return NULL;
         }
-
-        copy_a = 1;
     }
-
-    if (b->dtype.dtype != iot.intype){
-        b = Node_ToType(NULL, b, iot.intype);
-        if (!b){
-            if (copy_a){Node_Free(a);}
-            return NULL;
-        }
-        copy_b = 1;
-    }
-
-    Node* out;
-    if (c && c->dtype.dtype != iot.outtype){
-        out = NULL;
-        cast_out_to_c = 1;
-    }
-    else{
-        out = c;
-    }
-
-    if (intype != NDTYPE_FLOAT){
-        func_idx = (int)iot.intype;
-    }
-    else{
-        func_idx = (int)iot.intype - (int)NR_UINT64 - 1;
-    }
-
+    
+    /* Calculate function index */
+    int func_idx = (intype != NDTYPE_FLOAT) ? 
+        (int)iot.intype : 
+        (int)iot.intype - (int)NR_UINT64 - 1;
+    
+    /* Execute function */
     NFunc func = func_list[func_idx];
-
-    Node* in[] = {a, b};
-    NFuncArgs args = {
-        .nodes = in,
-        .out = out,
-        .outtype = iot.outtype,
-        .n_nodes = 2
-    };
-
+    Node* in[2] = {a, b};
+    NFuncArgs args;
+    args.nodes = in;
+    args.out = c;
+    args.outtype = iot.outtype;
+    args.n_nodes = b ? 2 : 1;
+    args.dim = 0;
+    args.extra = NULL;
+    
     int res = func(&args);
-
-    if (copy_a){
-        Node_Free(a);
-    }
-
-    if (copy_b){
-        Node_Free(b);
-    }
-
-    if (cast_out_to_c){
-        out = args.out;
-        args.out = Node_ToType(c, args.out, c->dtype.dtype);
-        Node_Free(out);
-    }
-
+    
+    /* Clean up */
+    _CleanupCopiedNodes(a, b, copy_a, copy_b);
+    
     return res != 0 ? NULL : args.out;
 }
 
+/* Wrapper functions for backward compatibility */
+NR_PRIVATE Node*
+_NMath_Func_2in1out(Node* c, Node* a, Node* b, NFunc func_list[], int outtype, int intype, int in_castable, char* func_name) {
+    return _NMath_Func_Core(c, a, b, func_list, outtype, intype, in_castable, func_name);
+}
 
 NR_PRIVATE Node*
 _NMath_Func_1in1out(Node* c, Node* a, NFunc func_list[], int outtype, int intype, int in_castable, char* func_name) {
-    if (!in_castable) {
-        int atype = NDtype_GetDtypeType(a->dtype.dtype);
-        if (atype != intype) {
-            char intype_str[20];
-            char adtype_str[20];
-            
-            NDtype_AsString(intype, intype_str);
-            NDtype_AsString(a->dtype.dtype, adtype_str);
-
-            NError_RaiseError(NError_TypeError, "%s operation not supported for input type %s. Expected input type %s.", 
-                func_name,
-                adtype_str,
-                intype_str
-            );
-            return NULL;
-        }
-    }
-
-    int copy_a = 0;
-    int cast_out_to_c = 0;
-    int func_idx;
-    __nmath_iotypes iot;
-
-    _CheckDtypes_2Nodes(&iot, c, a, outtype, intype);
-
-    if (a->dtype.dtype != iot.intype) {
-        a = Node_ToType(NULL, a, iot.intype);
-        if (!a) {
-            return NULL;
-        }
-        copy_a = 1;
-    }
-
-    Node* out;
-    if (c && c->dtype.dtype != iot.outtype) {
-        out = NULL;
-        cast_out_to_c = 1;
-    } else {
-        out = c;
-    }
-
-    if (intype != NDTYPE_FLOAT) {
-        func_idx = (int)iot.intype;
-    } else {
-        func_idx = (int)iot.intype - (int)NR_UINT64 - 1;
-    }
-
-    NFunc func = func_list[func_idx];
-
-    Node* in[] = {a};
-    NFuncArgs args = {
-        .nodes = in,
-        .out = out,
-        .outtype = iot.outtype,
-        .n_nodes = 1
-    };
-
-    int res = func(&args);
-
-    if (copy_a) {
-        Node_Free(a);
-    }
-
-    if (cast_out_to_c) {
-        out = args.out;
-        args.out = Node_ToType(c, args.out, c->dtype.dtype);
-        Node_Free(out);
-    }
-
-    return res != 0 ? NULL : args.out;
+    return _NMath_Func_Core(c, a, NULL, func_list, outtype, intype, in_castable, func_name);
 }
